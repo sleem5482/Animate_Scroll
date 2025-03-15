334c344b6e7772e81d9b6bb2878a593ec9d168d4
sleem <sleemhashem5482@gmail.com>
2025-03-15 16:09:08 +0200

    after add some changes in html and add comments

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
+//get my element
 let section = document.querySelector(".three");
 let spans =document.querySelectorAll(".progress span");
 
-
+//this function call when i am scrolling in my page
 window.onscroll=()=>{
-    if(window.scrollY>section.offsetTop-100){
+    if(window.scrollY >= section.offsetTop-100){//if size scroling of the window is greater than the size scrolling of the section is 
         spans.forEach((span)=>{
-            span.style.width=span.dataset.width;
+            span.style.width=span.dataset.width;//using data set in html and increasing the width with it
         })
     }
     else {
         spans.forEach((span)=>{
-            span.style.width=0;
+            span.style.width=0;//else reset the width
         })
     }
 }
